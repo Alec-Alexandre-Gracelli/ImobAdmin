76a946d7f7862674a25d43581c5cea1d3aba4249
Alec <alecgracelli@gmail.com>
2022-02-09 11:04:32 -0300

    Create Context and ConnectionStrings

---
+++ ImobAdmin/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ImobAdmin.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImobAdmin.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet <Imovel> Imoveis { get; set; }
+        public DbSet <Imagem> Imagens { get; set; }
+        public DbSet <Cidade> Cidades { get; set; }
+        public DbSet <Bairro> Bairros { get; set; }
+    }
+}
